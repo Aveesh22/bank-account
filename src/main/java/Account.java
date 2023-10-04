49ee6d0c7ffadd37b7686ebd243beafba45f1531
Aveesh22 <68564656+Aveesh22@users.noreply.github.com>
2023-10-04 14:11:37 -0400

    Add behavior to Account and AccountDatabase classes

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -4,4 +4,17 @@ public abstract class Account implements Comparable<Account>
     protected double balance;
     public abstract double monthlyInterest();
     public abstract double monthlyFee();
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    @Override
+    public boolean equals(Account acct)
+    {
+        /*if(holder.equals(acct.holder) && balance == acct.getBalance())
+            return true;
+        return false;*/
+    }
 }
--- src/main/java/AccountDatabase.java
+++ src/main/java/AccountDatabase.java
@@ -1,26 +1,77 @@
 public class AccountDatabase
 {
-    private Account [] accounts; //list of various types of accounts
+    private Account[] accounts; //list of various types of accounts
     private int numAcct; //number of accounts in the array
+
+    public static final int GROWTH_RATE = 4;
+
+    public static final int NOT_FOUND = -1;
+
+    public void setNumAccts()
+    {
+        int i = 0;
+        for(Account a : accounts)
+        {
+            if(a != null)
+            {
+                i++
+            }
+        }
+        this.numAcct = i;
+    }
+
+
     private int find(Account account) //search for an account in the array
     {
-
+        for(int i = 0; i < accounts.length; i++)
+        {
+            if(accounts[i].equals(account))
+                return i;
+        }
+        return -1;
     }
+
     private void grow() //increase the capacity by 4
     {
-
+        Account[] accounts1 = new Account[accounts.length + GROWTH_RATE];
+        for(int i = 0; i < accounts.length; i++)
+        {
+            accounts1[i] = accounts[i];
+        }
+        accounts = accounts1;
     }
+
     public boolean contains(Account account) //overload if necessary
     {
-
+        for(Account a: accounts)
+        {
+            if(a.equals(account))
+                return true;
+        }
+        return false;
     }
+
     public boolean open(Account account) //add a new account
     {
 
     }
+
     public boolean close(Account account) //remove the given account
     {
-
+        int index = find(account);
+        if (index != NOT_FOUND)
+        {
+            int i = index;
+            while (accounts[i] != null && i != numAcct)
+            {
+                accounts[i] = accounts[i + 1];
+                i++;
+            }
+            setNumAccts();
+            return true;
+        }
+        else
+            return false;
     }
     public boolean withdraw(Account account) //false if insufficient fund
     {
@@ -28,6 +79,7 @@ public class AccountDatabase
     }
     public void deposit(Account account)
     {
+        
 
     }
     public void printSorted() //sort by account type and profile
